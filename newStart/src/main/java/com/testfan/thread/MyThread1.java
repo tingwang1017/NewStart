a8016c976e038c6401320b87665887acde3a0656
tingtwang1017 <hnzzwtt@126.com>
2018-07-24 22:14:06 +0800

    thread

--- newStart/src/main/java/com/testfan/thread/MyThread1.java
+++ newStart/src/main/java/com/testfan/thread/MyThread1.java
@@ -1,9 +1,21 @@
 package com.testfan.thread;
 
 public class MyThread1 extends Thread{
-	
-	public void run () {
-		
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(3000);
+			System.out.println("run--"+Thread.currentThread().getName());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
+	public static void main (String[] args) {
+		MyThread1 mythread1 = new MyThread1();
+		mythread1.start();
+		System.out.println("main--"+Thread.currentThread().getName());
+	}
+	
 }
